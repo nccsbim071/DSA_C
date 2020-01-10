ea7d9c2e4c2452d9cf5b320c5aa8ec71a004ee9e
Randhir Kumar Yadav <yrandhir@outlook.com>
2020-01-10 17:08:27 +0545

    Committing workin in progress.

---
+++ 07_Array_Delete_Element.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+#define MAX 5 
+
+void main()
+{
+    int myArray[MAX] = {1,2,3,4,5}
+    int n = 5; 
+    int k = 3; 
+
+    printf("\nCurrent elements in array.");
+    for(int i=0;i<n;i++)
+    {
+        printf();
+    }
+}
