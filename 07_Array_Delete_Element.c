ef822d8728a639c00a06aab66462af5a1a7ab06c
Randhir Kumar Yadav <yrandhir@outlook.com>
2020-02-10 18:46:06 +0545

    Added 07_Array_Delete_Element.c file. This file consists of algorithm to delete element at given position and reorganizing all elements of an array.

--- 07_Array_Delete_Element.c
+++ 07_Array_Delete_Element.c
@@ -1,16 +1,37 @@
 #include <stdio.h> 
-
+#include <conio.h>
 #define MAX 5 
 
-void main()
+int main()
 {
-    int myArray[MAX] = {1,2,3,4,5}
+    int myArray[MAX] = {1,2,3,4,5};
     int n = 5; 
     int k = 3; 
 
-    printf("\nCurrent elements in array.");
+    printf("\nCurrent elements in array:\n");
+	
     for(int i=0;i<n;i++)
     {
-        printf();
+        printf("myArray[%d]:%d\n",i,myArray[i]);
     }
+	
+	int j=k;
+	
+	while(j<n)
+	{
+		myArray[j-1] = myArray[j];		
+		j++;
+	}
+	
+	n-=1;
+	
+	printf("\nElements in array after deleting item:\n");
+	
+    for(int i=0;i<n;i++)
+    {
+        printf("myArray[%d]:%d\n",i,myArray[i]);
+    }
+	
+	getch();
+	return 0;
 }
